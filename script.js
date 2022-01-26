18454d372d7915b39f19d8cea6f95e18a3f60256
user <86485005+Suhad77@users.noreply.github.com>
2022-01-26 11:21:11 +0600

    this is offcanvas

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelectors('.menu')
+
+
+// => Fat arrow
+// -> skiinny arrow
+
+const listenToMenuButton = () => {
+    console.log("I was clicked");
+}
+
+menu.addEventListener
