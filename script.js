d21c4a3591d983a785371b35a86728abfc264bca
Suhad77 <suhadmia12@gmail.com>
2022-01-28 23:57:48 +0600

    style change

--- script.js
+++ script.js
@@ -19,20 +19,19 @@ close.addEventListener('click',RemoveMenu);
 
 const navItems = [...document.querySelectorAll('.nav-items li a')]
 
-navItems.map(element => {
-    element.addEventListener('click', (e) => {
+navItems.map(element =>{
+    element.addEventListener('click', (e)=>{
         e.preventDefault();
-
         const id = e.target.getAttribute('href');
-        console.log(id);
-        const section =document.querySelector(id).offsetTop;
+        const section = document.querySelector(id).offsetTop;
         console.log(section);
+        
+            window.scroll({
+                top:section,
+                left:0,
+                behavior: 'smooth',
+            });
+    })
+})
 
 
-        // window.scroll({
-        //     top:0,
-        //     left:0,
-        //     behavior: 'smooth',
-        // })
-    });
-});
